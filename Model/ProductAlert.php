c9208df079804d17a675e40bed8e4ff8062e770c
Reshad Zazai <armirazai@gmail.com>
2021-06-11 12:48:50 +0430

    Adds price alert API support.
    Adds price alert support to the module-product-alert-api

--- Model/ProductAlert.php
+++ Model/ProductAlert.php
@@ -6,6 +6,7 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\ProductAlert\Model\StockFactory;
+use Magento\ProductAlert\Model\PriceFactory;
 
 use Pawan\ProductAlertApi\Api\ProductAlertManagementInterface;
 
@@ -14,21 +15,24 @@ class ProductAlert implements ProductAlertManagementInterface
     protected $productRepository;
     private $storeManager;
     protected $stockFactory;
+    protected $priceFactory;
 
     public function __construct(
         ProductRepositoryInterface $productRepository,
         StoreManagerInterface $storeManager,
-        StockFactory $stockFactory
+        StockFactory $stockFactory,
+        PriceFactory $priceFactory
     ) {
         $this->productRepository = $productRepository;
         $this->storeManager = $storeManager;
         $this->stockFactory = $stockFactory;
+        $this->priceFactory = $priceFactory;
     }
-    
+
     public function addProductAlertStock($customerId, $productId)
     {
         try {
-        
+
             /* @var $product \Magento\Catalog\Model\Product */
             $product = $this->productRepository->getById($productId);
             $store = $this->storeManager->getStore();
@@ -43,7 +47,7 @@ class ProductAlert implements ProductAlertManagementInterface
         } catch (NoSuchEntityException $noEntityException) {
             return false;
         }
-    } 
+    }
     public function deleteProductAlertStock ($customerId, $productId)
     {
         try {
@@ -70,5 +74,51 @@ class ProductAlert implements ProductAlertManagementInterface
             return false;
         }
     }
-    
+    public function addProductAlertPrice($customerId, $productId)
+    {
+        try {
+
+            /* @var $product \Magento\Catalog\Model\Product */
+            $product = $this->productRepository->getById($productId);
+            $store = $this->storeManager->getStore();
+            /** @var \Magento\ProductAlert\Model\Price $model */
+            $model = $this->priceFactory->create()
+                ->setCustomerId($customerId)
+                ->setProductId($product->getId())
+                ->setPrice($product->getPrice())
+                ->setWebsiteId($store->getWebsiteId())
+                ->setStoreId($store->getId());
+            $model->save();
+            return true;
+        } catch (NoSuchEntityException $noEntityException) {
+            return false;
+        }
+    }
+    public function deleteProductAlertPrice ($customerId, $productId)
+    {
+        try {
+            /* @var $product \Magento\Catalog\Model\Price */
+            $product = $this->productRepository->getById($productId);
+            $model = $this->priceFactory->create()
+                ->setCustomerId($customerId)
+                ->setProductId($product->getId())
+                ->setWebsiteId(
+                    $this->storeManager
+                        ->getStore()
+                        ->getWebsiteId()
+                )->setStoreId(
+                    $this->storeManager
+                        ->getStore()
+                        ->getId()
+                )
+                ->loadByParam();
+            if ($model->getId()) {
+                $model->delete();
+            }
+            return true;
+        } catch (NoSuchEntityException $noEntityException) {
+            return false;
+        }
+    }
+
 }
